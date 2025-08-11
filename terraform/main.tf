cf9dab8e654e33f6623279df70d665caf9f89e2c
priyadharshiniR07 <priyadharshinirajesh7@gmail.com>
2025-08-11 23:16:58 +0530

    Fix: Use identifier and db_name for aws_db_instance in Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,14 +19,14 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   key_name      = "mynewpair"   # <-- Add this line
 
-  user_data = <<-EOF
+ user_data = <<-EOF
     #!/bin/bash
     apt-get update -y
     apt-get install -y docker.io
     systemctl start docker
     systemctl enable docker
     usermod -aG docker ubuntu
-    docker run -d -p 80:80 --env DB_HOST=${aws_db_instance.app_db.address} --env DB_USER=admin --env DB_PASS=password123 priyadharshiniro7/fleet-management-app:latest
+    docker run -d -p 80:8000 --env DB_HOST=${aws_db_instance.app_db.address} --env DB_USER=admin --env DB_PASS=password123 priyadharshiniro7/fleet-management-app:latest
     EOF
 
   tags = {
